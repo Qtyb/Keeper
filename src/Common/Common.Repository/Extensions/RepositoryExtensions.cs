5997a476d8ca1d189119325ef6d5300f7a3567ed
Qtyb <1-qtyb@users.noreply.dev.gitlab.com>
2020-07-17 19:29:33 +0000

    feat: repositories added

---
+++ src/Common/Common.Repository/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Common.Repository.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Repository.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ src/Common/Common.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Common.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+
+        Task<T> GetById(long id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        void Delete(T entity);
+
+        void Add(T entity);
+    }
+}
